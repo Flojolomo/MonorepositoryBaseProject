6ca44952830058117e39214f71365a6d86aea1aa
Florian Siegel <florian.siegel@netlight.com>
2026-02-07 21:17:53 +0100

    fix: project settings with yarn_berry & npm deployment

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,11 +1,11 @@
 import { typescript } from "projen";
+import { NodePackageManager } from "projen/lib/javascript";
 const project = new typescript.TypeScriptProject({
   defaultReleaseBranch: "main",
   name: "@flocoder93/base-project",
   projenrcTs: true,
-  publishTasks: true,
-  npmTrustedPublishing: true,
-
+  releaseToNpm: true,
+  packageManager: NodePackageManager.YARN_BERRY,
   deps: ["projen"] /* Runtime dependencies of this module. */,
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   devDeps: ["@jest/globals"] /* Build dependencies for this module. */,
